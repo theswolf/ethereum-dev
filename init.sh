3af30ad5b6a832b4446ea53c75e10e982a2151e5
theswolf <chris.geymo@gmail.com>
2018-06-07 17:15:29 +0200

    first commit

--- init.sh
+++ init.sh
@@ -2,6 +2,12 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.bashrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 8
 npm install -g solc
 npm install -g truffle
 
